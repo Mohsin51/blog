fecdc4c70541ce70ac3b3016e1050d62b5e9a0e9
bilal <98596151+Mohsin51@users.noreply.github.com>
2022-03-12 15:51:12 +0500

    commit to blog

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Blog Post</h2>
+      {/* <h2>Blog Post</h2> */}
+      <h1>New Heading </h1>
       <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus temporibus deleniti omnis, tempora amet vero in blanditiis eos officia et, sed aliquid explicabo similique molestiae quo, veniam itaque adipisci sequi. </p>
+    <button>Hit Me Please</button>
     </div>
   );
 }
